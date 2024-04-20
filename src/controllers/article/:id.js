221143b49ad7abef415a34c26582a09fd883403b
yhl <1017935195@qq.com>
2024-04-20 13:14:22 +0800

    reder article details

--- src/controllers/article/:id.js
+++ src/controllers/article/:id.js
@@ -6,22 +6,15 @@ function thisYear(date){
     return moment().format('YYYY') === moment(date).format('YYYY')
 
 }
+
 function getArticle(id,cb){
-  const postData = {
-    id: id
-  }
   const options = {
     hostname:'127.0.0.1',
     port: 5166,
-    path: '/api/article/detail',
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(JSON.stringify(postData)),
-    },
-
+    path: '/api/article/detail?id='+ id,
+    method: 'get',
   }
-  const req = http.request(options ,(res)=>{
+  http.get(options ,(res)=>{
     let data = Buffer.alloc(0)
     res.on('data',(chunk)=>{
       data = Buffer.concat([data,chunk])
@@ -31,15 +24,12 @@ function getArticle(id,cb){
       cb(body)
     })
   })
-
-  req.write(JSON.stringify(postData));
-  req.end();
 }
 
 function index(req, res){
     const id = req.params.id
     getArticle(id ,(body)=>{
-        var e = body.data[0]
+        var e = body.data
         const createDate =  thisYear(e.created_date)? moment(e.created_date).format('MM-DD'):moment(e.created_date).format('YYYY-MM-DD')
         res.render('article', { 
                 title: e.title,
