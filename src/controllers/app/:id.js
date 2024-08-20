da716e770963a8777ea80e0c5d858bea27dcbe5a
yhl <1017935195@qq.com>
2024-08-21 00:00:50 +0800

    完成答题页面

--- src/controllers/app/:id.js
+++ src/controllers/app/:id.js
@@ -2,13 +2,13 @@ var axios = require('axios')
 
 function getList(id, cb) {
   var url = 'http://test.mbti.ink/mbti_api/questions?info_id=' + id
-  console.log(url);
   axios(url).then(e => {
     cb(e.data)
   })
 }
 
 function index(req, res) {
+  let goods_id = req.query.id;
   let id = req.params.id;
   getList(id, (body) => {
     var data = body.data
@@ -16,6 +16,8 @@ function index(req, res) {
     res.render('id', {
       name: data.name,
       list: data.list,
+      goods_id: goods_id, 
+      info_id: id
     });
   })
 }
---
+++ src/controllers/wait-pay/index.js
@@ -0,0 +1,9 @@
+
+function index(req, res){
+    const id = req.query.id
+    res.render('wait-pay', { 
+           
+    });
+}
+
+module.exports = index
