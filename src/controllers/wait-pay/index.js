8824b7470145dff2c3f6df6a11093601c7cf395b
yhl <1017935195@qq.com>
2024-09-06 00:44:49 +0800

    style

--- src/controllers/wait-pay/index.js
+++ src/controllers/wait-pay/index.js
@@ -1,9 +1,18 @@
+var axios = require('axios')
+function getPrice(id, cb){
+    var url = 'http://test.mbti.ink/mbti_api/order/prices?order_id='+ id
+    axios(url).then(e => {
+      cb(e.data)
+    })
+  }
 
 function index(req, res){
     const id = req.query.id
-    res.render('wait-pay', { 
-           
-    });
+    getPrice(id, function(res){
+        res.render('wait-pay', { 
+            list: res.data
+        });
+    })
 }
 
 module.exports = index
