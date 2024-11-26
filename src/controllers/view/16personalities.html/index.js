eef0bc2e3377434b8a2d1ffb2545f07458eec25c
hongli <yan@honglis-Mac-mini.local>
2024-11-27 01:27:29 +0800

    优化代码结构，添加人格类型动画

--- src/controllers/view/16personalities.html/index.js
+++ src/controllers/view/16personalities.html/index.js
@@ -4,32 +4,36 @@ const typesGroup = [
     title_locale: '分析家',
     types: [{
       type: 'INTJ',
-      name: '建筑师',
+      name_local: '建筑师',
+      name: 'architect',
       snippet: '富有想象力和战略性思维，一切皆在计划之中。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/intj-architect.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/architect.json'
       
     },
     {
       type: 'INTP',
-      name: '逻辑学家',
+      name_local: '逻辑学家',
+      name: 'logician',
       snippet: '具有创造力的发明家，对知识有着不可抑制的渴望。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/intp-logician.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/logician.json'
     },
     {
       type: 'ENTJ',
-      name: '指挥官',
+      name_local: '指挥官',
+      name: 'commander',
       snippet: '大胆、富有想象力且意志强大的领导者，总能找到或创造解决方法。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/entj-commander.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/commander.json'
     },
     {
       type: 'ENTP',
-      name: '辩论家',
+      name_local: '辩论家',
+      name: 'debater',
       snippet: '聪明好奇的思考者，无法抵挡智力挑战的诱惑。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/entp-debater.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/debater.json'
     },
   ]
   },
@@ -38,31 +42,35 @@ const typesGroup = [
     title_locale: '外交家',
     types: [{
       type: 'INFJ',
-      name: '提倡者',
+      name_local: '提倡者',
+      name: 'advocate',
       snippet: '安静神秘，但非常鼓舞人心且不知疲倦的理想主义者。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/infj-advocate.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/advocate.json'
     },
     {
       type: 'INFP',
-      name: '调停者',
+      name_local: '调停者',
+      name: 'mediator',
       snippet: '富有诗意、善良且无私的人，总是热衷于帮助正义事业。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/infp-mediator.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/mediator.json'
     },
     {
       type: 'ENFJ',
-      name: '主人公',
+      name_local: '主人公',
+      name: 'protagonist',
       snippet: '具有魅力并能激励人心的领导者，能够让听众为之着迷。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/enfj-protagonist.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/protagonist.json'
     },
     {
       type: 'ENFP',
-      name: '竞选者',
+      name_local: '竞选者',
+      name: 'campaigner',
       snippet: '充满活力，富有创意，善于交际的自由之人，总能找到微笑的理由。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/enfp-campaigner.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/campaigner.json'
     },]
   },
   {
@@ -71,31 +79,35 @@ const typesGroup = [
     types: [
       {
       type: 'ISTJ',
-      name: '物流师',
+      name_local: '物流师',
+      name: 'logistician',
       snippet: '务实且注重事实的人，可靠性不容怀疑。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/istj-logistician.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/logistician.json'
     },
       {
       type: 'ISFJ',
-      name: '守卫者',
+      name_local: '守卫者',
+      name: 'defender',
       snippet: '非常专注和热情的保护者，总是随时准备保护他们所爱的人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/isfj-defender.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/defender.json'
     },
       {
       type: 'ESTJ',
-      name: '总经理',
+      name_local: '总经理',
+      name: 'executive',
       snippet: '出色的管理者，在管理事物或人的方面无与伦比。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/estj-executive.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/executive.json'
     },
       {
       type: 'ESFJ',
-      name: '执政官',
+      name_local: '执政官',
+      name: 'consul',
       snippet: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/esfj-consul.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/consul.json'
     }
   ]
   },
@@ -104,35 +116,39 @@ const typesGroup = [
     title_locale: '探险家',
     types: [{
       type: 'ISTP',
-      name: '鉴赏家',
+      name_local: '鉴赏家',
+      name: 'virtuoso',
       snippet: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/istp-virtuoso.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/virtuoso.json'
     },{
       type: 'ISFP',
-      name: '探险家',
+      name_local: '探险家',
+      name: 'adventurer',
       snippet: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/isfp-adventurer.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/adventurer.json'
     },{
       type: 'ESTP',
-      name: '企业家',
+      name_local: '企业家',
+      name: 'entrepreneur',
       snippet: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/estp-entrepreneur.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/entrepreneur.json'
     },{
       type: 'ESFP',
-      name: '表演者',
+      name_local: '表演者',
+      name: 'entertainer',
       snippet: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
       img:'https://www.16personalities.com/static/images/personality-types/avatars/esfp-entertainer.svg', 
-      animation:''
+      animation:'/animations/avatars/defaults/entertainer.json'
     }]
   },
 ]
 
 function index(req, res) {
   res.render('16personalities', {
-
+    typesGroup:typesGroup
   });
 }
 module.exports = index
