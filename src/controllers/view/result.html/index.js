b0c87bfaea9deb3add3796ac2fb032db2f1b46e0
peiili <1017935195@qq.com>
2025-01-13 21:20:18 +0800

    添加文章列表

---
+++ src/controllers/view/result.html/index.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const moment = require('moment')
+const marked = require('marked')
+
+function thisYear(date){
+    return moment().format('YYYY') === moment(date).format('YYYY')
+
+}
+
+function getArticle(id,cb){
+  const options = {
+    hostname:'127.0.0.1',
+    port: 5166,
+    path: '/api/article/detail?id='+ id,
+    method: 'get',
+  }
+  http.get(options ,(res)=>{
+    let data = Buffer.alloc(0)
+    res.on('data',(chunk)=>{
+      data = Buffer.concat([data,chunk])
+    })
+    res.on('end',()=>{
+      const body = JSON.parse(data.toString())
+      cb(body)
+    })
+  })
+}
+
+function index(req, res){
+    const id = req.params.id
+    getArticle(id ,(body)=>{
+        var e = body.data
+        const createDate =  thisYear(e.created_date)? moment(e.created_date).format('MM-DD'):moment(e.created_date).format('YYYY-MM-DD')
+        res.render('article', {
+                title: e.title,
+                createDate: createDate,
+                description: e.description,
+                keywords: e.keywords,
+                html: marked.parse(e.marked||'')
+            });
+    })
+}
+
+module.exports = index
--- src/template/index/static/tabbar.js
+++ src/template/index/static/tabbar.js
@@ -10,11 +10,11 @@ const loadTabbar = () => {
       <img class="tabbar_item_icon" src="static/img/tabbar_2.png">
       <p class="tabbar_item_title">类型</p>
     </a>
-    <a class="tabbar_item" href="javascript:void(0)" onclick="jumpStandard()">
+    <a class="tabbar_item" href="/app/1.html">
       <img class="tabbar_item_icon" src="./img/tabbar_3.png">
       <p class="tabbar_item_title">标准版测试</p>
     </a>
-    <a class="tabbar_item" href="javascript:void(0)" onclick="jumpProfessional()">
+    <a class="tabbar_item" href="/app/8.html">
       <img class="tabbar_item_icon" src="./img/tabbar_4.png">
       <p class="tabbar_item_title">专业版测试</p>
     </a>
@@ -24,18 +24,6 @@ const loadTabbar = () => {
   document.querySelector('body').appendChild(tabbarDiv)
   document.querySelector('body').appendChild(tabbarHolderDiv)
 }
-// 标准版
-function jumpStandard() {
-  let msg='你是否愿意测试：更准确的知己专业版MBTI呢？'
-  if (confirm(msg) == true) {
-    window.location.href = 'https://iqeq.com.cn/mbtics-v2.html?vip'
-  } else {
-    window.location.href = '/mbti16go.aspx'
-  }
-}
-// 专业版
-function jumpProfessional() {
-  window.location.href = 'https://iqeq.com.cn/mbtics-v2.html?vip'
-}
+
 
 // window.addEventListener('load', loadTabbar)
